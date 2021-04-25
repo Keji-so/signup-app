37357fba106c411e5b38f999a8099aaa93a5691c
kejiojo <ojokeji@gmail.com>
2021-04-25 10:01:02 +0100

    updated data

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const router = express.Router() //import router from express
 const signUpTemplateCopy = require('../tables/SignUpTables') // import table created with mongoose, this is needed to collect the info submitted
+const bcrypt = require('bcrypt') //used to hide password
 
 
-router.post('/signup', (req, res) => {  //path for post request from form
+router.post('/signup', async (req, res) => {  //path for post request from form (async is used here cos we have to wait for the password to be encrypted first before sending)
+
+    const saltPassword = await bcrypt.genSalt(10)
+    const securePassword = await bcrypt.hash(req.body.password, saltPassword)
+
     const signedUpUser = new signUpTemplateCopy({
         fullName: req.body.fullName, //grab fullname from body of the request
         userName: req.body.userName,
         email: req.body.email,
-        password: req.body.password
+        password: securePassword
     })
     signedUpUser.save() // save the data collected
       .then(data => {
--- src/App.js
+++ src/App.js
@@ -1,37 +1,88 @@
-
+import React, {useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+const [ fullName, setFullName] = useState("")
+const [ userName, setUserName] = useState("")
+const [ email, setEmail] = useState("")
+const [ password, setPassword] = useState("")
+
+const handleFullName = (e) => {
+    setFullName(e.target.value)
+}  
+  
+  const handleUserName = (e) => {
+    setUserName(e.target.value)
+}  
+
+const handleEmail = (e) => {
+  setEmail(e.target.value)
+}  
+
+const handlePassword = (e) => {
+  setPassword(e.target.value)
+}  
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+
+  const registered = {
+    fullName: fullName,
+    userName: userName,
+    email: email,
+    password: password
+  }
+
+//axios will be used to send the info stored in "registered" to the backend
+
+  axios.post('http://localhost:4000/app/signup', registered) //this means post 'registered' to 'http://localhost:4000/app/signup'
+    // .then(response => console.log(response.data))
+
+  //after submitting we can set the page we want to be opened e.g if we want homepage to open after use "windows.location = '/home' "  
+
+  setFullName('')
+  setUserName('')
+  setEmail('')
+  setPassword('')
+
+  //reset all the fields
+}
+
+
+
   return (
     <div className="App">
      
     <div className="form-div left">
       <div className="sign-in">Already a member? <span>Sign in</span></div>
-      <form className="form" action="">
+
+      <form className="form" onSubmit={handleSubmit} action="">
         
         <h1>Sign Up</h1>
 
       <div className="names">
         <div className="name-field">
           <div className="label">Name</div>
-          <input type="text" placeholder="enter name"/>
+          <input onChange={handleFullName} value={fullName} type="text" placeholder="enter name"/>
         </div>
 
         <div className="name-field">
           <div className="label">Username</div>
-          <input type="text" placeholder="@username" />
+          <input onChange={handleUserName} value={userName} type="text" placeholder="@username" />
         </div>
       </div>
         
 
         <div className="e-mail">
           <div className="label">E-mail</div>
-          <input type="text" placeholder="name@example.com" />
+          <input onChange={handleEmail} value={email} type="text" placeholder="name@example.com" />
         </div>
 
         <div className="password">
           <div className="label">Password</div>
-          <input type="password" placeholder="*****" />
+          <input onChange={handlePassword} value={password} type="password" placeholder="*****" />
         </div>
 
         <div className="button">
